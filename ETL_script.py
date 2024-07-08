fd1c3f3e75c5c9e83b6c097171480f6d59adb38f
Eren Tunc Cetin <erentunccetin@gmail.com>
2024-07-08 17:37:00 +0300

    postgres databasesi kuruluyor buradan sonra yapılacaklar: mysql databasesi kur postgresten mysql'e yolla

--- ETL_script.py
+++ ETL_script.py
@@ -1,17 +1,19 @@
 import psycopg2
 import time
+
 RETRY_ATTEMPTS = 20
-RETRY_DELAY = 5  # in seconds
+RETRY_DELAY = 5
 
 for attempt in range(RETRY_ATTEMPTS):
     try:
         db = psycopg2.connect(
-            host="db",
-            port="5432", 
-            database="test_db",
-            user="root",
-            password="root"
+            database = "test_db",
+            user = "root",
+            password = "root",
+            host = "db",
+            port = "5432"
         )
+        print("BAGLANDI")
         break
     except psycopg2.OperationalError as e:
         print(f"Attempt {attempt + 1} failed: {e}")
@@ -38,16 +40,20 @@ create_table_queries = [
     """,
     """
     CREATE TABLE IF NOT EXISTS table3 (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    username VARCHAR(255)
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255),
+        username VARCHAR(255)
     );
-    
     """
 ]
+
 with db.cursor() as cursor:
     for query in create_table_queries:
         try:
             cursor.execute(query)
+            print(f"Executed query: {query}")
         except psycopg2.Error as e:
-            print(e)
+            print(f"Error executing query: {e}")
+
+db.commit()  # Ensure changes are committed
+db.close()  # Close the connection
